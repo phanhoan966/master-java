fddf6a4e82513e147cfe47fdba0b0c051fb75fbd
unknown <phanhoan.vnu@gmail.com>
2022-06-30 21:33:59 +0700

    first commit

---
+++ src/main/java/com/master_java/MasterJavaApplication.java
@@ -0,0 +1,14 @@
+package com.master_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterJavaApplication.class, args);
+		System.out.println("Project started");
+	}
+
+}
---
+++ src/test/java/com/master_java/MasterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
